1419c395c7ff1209a82ade2e32c1f802a7c3019a
Савелий Мотов <motov.s@edu.narfu.ru>
2021-02-22 21:50:21 +0300

    Сделал нормальный проект

--- ROV/main.cpp
+++ ROV/main.cpp
@@ -1,8 +1,42 @@
-﻿#include <cstdio>
-#include "IntMatrixclass.hpp"
+#pragma once
 
-int main()
-{
-    printf("Вас приветствует %s!\n", "ROV");
+#include <iostream>
+
+#include "DataStruct.hpp"
+#include "Socket.hpp"
+
+#include "IntMatrixClass.hpp"
+#include "FloatMatrixClass.hpp"
+
+
+using namespace std;
+
+int main(void) {
+
+    CommandsStruct* commandsStruct = new CommandsStruct;
+    MotorsStruct* motorsStruct = new MotorsStruct;
+
+
+    char motorsMessage[MotorsStructLenMessage] = {};
+
+
+    Socket socket;
+    socket.MakeServerSocket(1999);
+    
+
+
+    for (;;) 
+    {
+        while (socket.GetSocketConnectionStatus()) {
+            cout << "Hello World\n";
+        }
+
+        socket.Listen();
+    }
+
+    delete commandsStruct;
+    delete motorsStruct;
+
     return 0;
 }
+
