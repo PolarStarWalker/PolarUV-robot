6c98dea321146ba92bd4172d210887edf4ad5c60
motov.s <motov.s@edu.narfu.ru>
2022-02-13 18:26:33 +0300

    Добавил пайплайны для автономного робота

--- src/Robot/Features/Video/Video.cpp
+++ src/Robot/Features/Video/Video.cpp
@@ -5,6 +5,23 @@
 
 using namespace app;
 
+
+namespace auv {
+    constexpr std::string_view auv_pipeline = R"(v4l2src device=/dev/video2 do-timestamp=true
+        ! video/x-h264,width=1920,height=1080,framerate=30/1
+        ! h264parse
+        ! v4l2h264dec
+        ! autovideoconvert
+        ! appsink sync=false)";
+
+    constexpr std::string_view auv_stream = R"(appsrc
+        ! videoconvert
+        ! v4l2h264enc output-io-mode=4  extra-controls="encode,frame_level_rate_control_enable=1,h264_profile=0,h264_level=14,video_bitrate=2500000;"
+        ! h264parse
+        ! matroskamux
+        ! filesink location=/home/pi/files/test.mkv)";
+}
+
 Video::Video(ssize_t id) : IService(id), childPid_() {}
 
 inline void Video::StartVideo(const std::string &pipeline) {
